a3d0c25dd8cad3783289d0a00b9afbe9beff3d78
famesensor <famfew25@gmail.com>
2020-04-05 15:46:57 +0700

    :sparkles: test println type variable

---
+++ main.go
@@ -0,0 +1,17 @@
+package main	
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi, amam")
+	fmt.Println("1 + 1 =",1+1)
+	fmt.Println("1.0 + 1.0 =",1.0+1.0)
+	fmt.Println(len("Hello world"))
+	fmt.Println("HelloWorld"[1])
+	fmt.Println(`Hi`)
+	fmt.Println("Hello"+"Hello")
+	fmt.Println(`Hi`+`Hi`)
+	fmt.Println(true && true)
+	fmt.Println(false || false)
+	fmt.Println(!false)
+}
