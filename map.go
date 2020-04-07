112d65f100ceed11e71c88e3f7a5996c86f73252
famesensor <famfew25@gmail.com>
2020-04-07 20:37:03 +0700

    :tada: map go

---
+++ map.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func maps() {
+	x := make(map[string]int)
+	x["key"] = 10
+	fmt.Println(x["key"])
+
+	y := make(map[int]int)
+	y[1] = 10
+	fmt.Println(y[1])
+
+	delete(y, 1)
+	fmt.Println(len(y))
+
+	elements := map[string]string{
+		"H":  "Hydrogen",
+		"He": "Helium",
+		"Li": "Lithium",
+		"Be": "Beryllium",
+		"B":  "Boron",
+		"C":  "Carbon",
+		"N":  "Nitrogen",
+		"O":  "Oxygen",
+		"F":  "Fluorine",
+		"Ne": "Neon",
+	}
+	fmt.Println(elements["Un"])
+
+	name, ok := elements["Un"]
+	fmt.Println(name, ok)
+}
